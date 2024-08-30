9166f1f84de73eb1e8ebddbf26aaed4de14a1fb1
Aleksandr <aleksandr.suslov@innowise.com>
2024-08-30 16:47:53 +0200

    feat: add exeption filter for prisma client

--- backend/users/src/main.ts
+++ backend/users/src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClientExceptionFilter } from './prisma/prisma-client-exeption.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   const configService = app.get(ConfigService);
   const port = configService.get('USERS_SERVICE_PORT');

bc5b3511238961e72d77c1b61dcea83a3c45f1bf
M-Ziusko <mikhail.ziusko@innowise.com>
2024-09-09 12:48:15 +0300

    feat(`config`): add validation for `env` variables (#19)
    * feat(`config`): add validation for `env` variables
    
    * fix: address PR review changes
    
    Co-authored-by: foobazdev <aleksandr.suslov@innowise.com>
    
    ---------
    
    Co-authored-by: foobazdev <aleksandr.suslov@innowise.com>

--- backend/users/src/app.module.ts
+++ backend/users/src/app.module.ts
@@ -3,9 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AppService } from './app.service';
+import { validationSchema } from './validation/config.schema';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

9b941d283268721d2ab8e7c58789ccfb99bfe178
M-Ziusko <mikhail.ziusko@innowise.com>
2024-09-09 11:43:52 +0200

    fix: address PR review changes
    Co-authored-by: foobazdev <aleksandr.suslov@innowise.com>

--- backend/users/src/validation/config.schema.ts
+++ backend/users/src/validation/config.schema.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
 
   // ### USERS MICROSERVICE CONFIG
   USERS_SERVICE_PORT: Joi.number().port().default(4000),
